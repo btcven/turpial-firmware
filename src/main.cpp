18e31bf2c6b7ba9c33a7bf0da26ff5f31e7527a3
luisan00 <luisan00@hotmail.com>
2019-09-11 19:22:27 +0200

    initial structure

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Locha Mesh Developers (contact@locha.io)
+ * @brief 
+ * @version 0.1
+ * @date 2019-09-11
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include <Arduino.h>
+
+void setup(){
+    //
+}
+
+void loop(){
+    //
+}
