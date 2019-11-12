236dcc077d7c23bf18058d87fa74037b87c527b8
Jean Pierre Dudey <jeandudey@hotmail.com>
2019-11-11 23:00:26 -0500

    add more tests for String class

--- test/TestString.cpp
+++ test/TestString.cpp
@@ -7,7 +7,31 @@
 #include "TinyString.h"
 
 /**
- * @brief Serialize and deserialize a char* with SerializablePOD class.
+ * @brief Test the String class constructors
+ * 
+ */
+void test_construct_string(void) {
+    auto test_string_1 = tinystring::String("LOCHA.IO");
+    TEST_ASSERT_EQUAL_STRING("LOCHA.IO", test_string_1.c_str());
+
+    auto test_string_2 = tinystring::String();
+    TEST_ASSERT_EQUAL_PTR(nullptr, test_string_2.c_str());
+}
+
+/**
+ * @brief Test the String class length() function
+ * 
+ */
+void test_length_string(void) {
+    auto test_string_1 = tinystring::String("LOCHA.IO");
+    TEST_ASSERT_EQUAL_INT(8, test_string_1.length());
+
+    auto test_string_2 = tinystring::String();
+    TEST_ASSERT_EQUAL_PTR(nullptr, test_string_2.c_str());
+}
+
+/**
+ * @brief Serialize and deserialize a String
  * 
  */
 void test_serialize_deserialize_string(void) {
@@ -30,6 +54,8 @@ extern "C" void app_main() {
     delay(2000);
 
     UNITY_BEGIN();
+    RUN_TEST(test_construct_string);
+    RUN_TEST(test_length_string);
     RUN_TEST(test_serialize_deserialize_string);
     UNITY_END();
 }
