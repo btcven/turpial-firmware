de15b05810842c81becea84d17d54f151aa065ab
Jean Pierre Dudey <jeandudey@hotmail.com>
2019-11-19 18:54:13 -0500

    update test_wifi_mode

--- test/test_wifi_mode/test_wifi_mode.cpp
+++ test/test_wifi_mode/test_wifi_mode.cpp
@@ -8,47 +8,48 @@
  * 
  */
 
-
 #include "WiFiMode.h"
 #include "defaults.h"
 #include "sdkconfig.h"
-#include <Arduino.h>
+
 #include <cstdint>
 #include <sstream>
 #include <unity.h>
 
-
-void set_default_wifi_params(wifi::DTOConfig& wifi_params)
+void setDefaultWiFiParams(wifi::DTOConfig& wifi_params)
 {
     wifi_params.ap_channel = WAP_CHANNEL;
     wifi_params.ap_max_conn = WAP_MAXCONN;
-    wifi_params.wap_enabled = WAP_ENABLED;
-    wifi_params.wst_enabled = WST_ENABLED;
+    wifi_params.wifi_mode = WIFI_MODE;
     wifi_params.is_open = false;
-    wifi_params.ap_ssid = tinystring::String(WAP_SSID);
-    wifi_params.ap_password = tinystring::String(WAP_PASS);
-    wifi_params.wst_ssid = tinystring::String(WST_SSID);
-    wifi_params.wst_password = tinystring::String(WST_PASS);
+    wifi_params.ap_ssid = WAP_SSID;
+    wifi_params.ap_password = WAP_PASS;
+    wifi_params.wst_ssid = WST_SSID;
+    wifi_params.wst_password = WST_PASS;
 }
 
-void test_wifi_mode_with_default_values(void)
+void testWifiMode(void)
 {
     const TickType_t xDelay = 10000 / portTICK_PERIOD_MS;
+
     wifi::DTOConfig wifi_parameters;
-    set_default_wifi_params(wifi_parameters);
-    std::cout << "wst_ssid: " << wifi_parameters.wst_ssid.c_str() << std::endl;
-    std::cout << "wst_password: " << wifi_parameters.wst_password.c_str() << std::endl;
-    std::cout << "wap_enabled: " << wifi_parameters.wap_enabled << std::endl;
-    std::cout << "wst_enabled: " << wifi_parameters.wst_enabled << std::endl;
-    wifi::mode::begin(wifi_parameters);
+    setDefaultWiFiParams(wifi_parameters);
+
+    wifi::WiFiMode wifi_mode;
+    esp_err_t err = wifi_mode.begin(wifi_parameters);
+    if (err != ESP_OK) {
+        ESP_LOGE(__func__, "Couldn't start WiFi interface (%s)", esp_err_to_name(err));
+        return;
+    }
+
     vTaskDelay(xDelay);
 }
 
-
 extern "C" void app_main()
 {
-    initArduino();
+    vTaskDelay(2000);
+
     UNITY_BEGIN();
-    RUN_TEST(test_wifi_mode_with_default_values);
+    RUN_TEST(testWifiMode);
     UNITY_END();
 }
