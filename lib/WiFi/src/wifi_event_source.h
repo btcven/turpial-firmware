24825b8523279b58d167f001cef0ccf9e7108387
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2019-11-24 23:40:53 -0500

    add some changes in events capture

--- lib/WiFi/src/wifi_event_source.h
+++ lib/WiFi/src/wifi_event_source.h
@@ -54,42 +54,6 @@ enum {
 extern "C" {
 #endif
 
-// This example makes use of two event sources: a periodic timer, and a task.
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Declarations for event source 1: periodic timer
-#define TIMER_EXPIRIES_COUNT        3        // number of times the periodic timer expires before being stopped
-#define TIMER_PERIOD                1000000  // period of the timer event source in microseconds
-
-extern esp_timer_handle_t g_timer;           // the periodic timer object
-
-// Declare an event base
-ESP_EVENT_DECLARE_BASE(TIMER_EVENTS);        // declaration of the timer events family
-
-enum {                                       // declaration of the specific events under the timer event family
-    TIMER_EVENT_STARTED,                     // raised when the timer is first started
-    TIMER_EVENT_EXPIRY,                      // raised when a period of the timer has elapsed
-    TIMER_EVENT_STOPPED                      // raised when the timer has been stopped
-};
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// Declarations for event source 2: task
-#define TASK_ITERATIONS_COUNT        5       // number of times the task iterates
-#define TASK_ITERATIONS_UNREGISTER   3       // count at which the task event handler is unregistered
-#define TASK_PERIOD                  500     // period of the task loop in milliseconds
-
-ESP_EVENT_DECLARE_BASE(TASK_EVENTS);         // declaration of the task events family
-
-enum {
-    TASK_ITERATION_EVENT,                    // raised during an iteration of the loop within the task
-};
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Declarations for event source 2: task
-/* #define TASK_ITERATIONS_COUNT        5       // number of times the task iterates
-#define TASK_ITERATIONS_UNREGISTER   3       // count at which the task event handler is unregistered
-#define TASK_PERIOD                  500     // period of the task loop in milliseconds
- */
 ESP_EVENT_DECLARE_BASE(WIFI_EVENTS);         // declaration of the task events family
 
 enum {
