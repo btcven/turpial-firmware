f76b4857445abc7e8126d2f0195f8fb3f2411975
Jean Pierre Dudey <jeandudey@hotmail.com>
2019-11-27 12:06:01 -0500

    document Semaphore class

--- lib/Util/src/Sema.h
+++ lib/Util/src/Sema.h
@@ -17,10 +17,25 @@
 
 namespace util {
 
+/**
+ * @brief A FreeRTOS Semaphore abstraction
+ * 
+ */
 class Semaphore
 {
 public:
+    /**
+     * @brief Construct a new
+     * 
+     * @param dbg_name: name of the semaphore for debugging purposes
+     */
     Semaphore(const char* dbg_name);
+
+    /**
+     * @brief Construct a new Semaphore from other moved Semaphore
+     * 
+     * @param other 
+     */
     Semaphore(Semaphore&& other);
 
     ~Semaphore();
@@ -29,14 +44,33 @@ public:
     Semaphore& operator=(Semaphore const&) = delete;
     Semaphore& operator=(Semaphore&&) = delete;
 
+    /**
+     * @brief Take or "lock" this semaphore
+     * 
+     */
     void take();
+
+    /**
+     * @brief Give or "unlock" this semaphore
+     * 
+     */
     void give();
+
+    /**
+     * @brief Waits for this semaphore to be "given".
+     * 
+     */
     void wait();
 
 private:
+    /**
+     * @brief Construct a new Semaphore
+     * 
+     */
     Semaphore() {}
-    SemaphoreHandle_t m_handle;
-    const char* m_dbg_name;
+
+    SemaphoreHandle_t m_handle; /**< raw semaphore handle */
+    const char* m_dbg_name;     /**< debug name */
 };
 
 } // namespace util
