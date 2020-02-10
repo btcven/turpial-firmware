c654234492575d1cc1602af2785ca0e1212e62c1
luisan00 <luisan00@MBP-de-luisan00.home>
2020-02-10 22:54:53 +0100

    Add codespell scripts

--- dist/tools/codespell/check.sh
+++ dist/tools/codespell/check.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+CODESPELL_CMD="codespell"
+CODESPELL_OPTS=" -q 2"
+CODESPELL_OPTS+=" -c"
+CODESPELL_OPTS+=" --check-hidden"
+CODESPELL_OPTS+=" --ignore-words-list=ND,nd,wan,od,dout"
 
-INCLUDE_DIRS="./components"
-CS_OPTIONS="-S"
+ERRORS=$(${CODESPELL_CMD} ${CODESPELL_OPTS})
 
+if [ -n "${ERRORS}" ]
+then
+    echo "${ERRORS}"
+    printf "Found some misspellings words, please fix it before continue.\n"
+    exit 1
+else
+    printf "Codespell return no errors.\n"
+    exit 0
+fi
