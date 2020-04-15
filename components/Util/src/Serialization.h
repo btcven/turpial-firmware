76bacc47e824f62e35d9058a700743121065b77f
xkevin190 <kevinvelasco193@gmail.com>
2020-04-15 12:28:44 -0400

    suggested changes

--- components/Util/src/Serialization.h
+++ components/Util/src/Serialization.h
@@ -4,7 +4,7 @@
  * @author Locha Mesh Developers (contact@locha.io)
  * @brief 
  * @version 0.1
- * @date 2020-04-11
+ * @date 2020-04-14
  * 
  * @copyright Copyright (c) 2020 Locha Mesh project developers
  * @license Apache 2.0, see LICENSE file for details
