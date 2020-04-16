9aa30a60cc88df39cbac40c6c8b0cff4cdf9bab8
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-04-16 14:35:17 -0500

    components/HTTP: fix documentation on Websocket.{h,cpp}
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- components/HTTP/src/Websocket.cpp
+++ components/HTTP/src/Websocket.cpp
@@ -1,18 +1,19 @@
 /**
  * @file Websocket.cpp
  * @author Locha Mesh Developers (contact@locha.io)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2020-03-25
- * 
+ *
  * @copyright Copyright (c) 2020 Locha Mesh Developers
- * 
+ *
  */
 
 #include "Websocket.h"
 #include "CheckConnections.h"
 #include <esp_log.h>
 #include <iostream>
+
 static const char* TAG = "Websocket";
 
 
@@ -44,7 +45,7 @@ static void receiveFromUart(void* data_received, void* length)
     // send message to customers
 }
 
-void Websocket::initRadioSerialLine(void) 
+void Websocket::initRadioSerialLine(void)
 {
     radio_task = new radio::Radio();
     radio_task->init(receiveFromUart);
--- components/HTTP/src/Websocket.h
+++ components/HTTP/src/Websocket.h
@@ -1,12 +1,12 @@
 /**
  * @file Websocket.h
  * @author Locha Mesh Developers (contact@locha.io)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2020-03-25
- * 
+ *
  * @copyright Copyright (c) 2020 Locha Mesh Developers
- * 
+ *
  */
 
 
@@ -29,13 +29,13 @@
 
 
 /**
- * @brief customer structure
+ * @brief Customer data structure
  */
 struct client_data_t {
-    std::uint8_t shaUID[32]; // customer id
-    int timestamp;           // time the connection was made
-    bool is_alive;           // indicates if the connection is alive or not
-    int fd;                  // connection identifier
+    std::uint8_t shaUID[32]; /**< Customer id */
+    int timestamp; /**< Time the connection was made */
+    bool is_alive; /**< Indicates if the connection is alive or not */
+    int fd; /**< Connection identifier */
 };
 
 /**
@@ -54,16 +54,15 @@ struct async_resp_arg_t {
 };
 
 /**
- * @brief uid of incoming messages
+ * @brief UID of incoming messages
  */
 struct uid_message_t {
-    std::uint8_t from_uid[32]; // uid who's sending the message
-    std::uint8_t to_uid[32];   // id of who receives the message
+    std::uint8_t from_uid[32]; /**< UID who's sending the message */
+    std::uint8_t to_uid[32]; /**< UID of who's receving the message */
 };
 
 
-class Websocket
-{
+class Websocket {
 public:
     Websocket(Websocket const&) = delete; // Copy construct
     Websocket(Websocket&&) = delete;      // Move construct
@@ -96,32 +95,35 @@ public:
     void checkConnection();
 
 private:
-    /**
-   * @brief check active customers
-   *  
-   */
+   /**
+    * @brief check active customers
+    *
+    */
     Websocket();
 
 
     std::vector<client_data_t> m_client; // array of connected clients
 
     httpd_req_t* req_handler; // http_esp_server connection handler
+
     radio::Radio* radio_task;
 
     /**
-     * @brief  get message type
-     * @param  payload data sent by the client
-     * @return 
-     * 0 : when the message type is handshakey 
-     * 1 : when the message type is message
-     * 2 : when the message type is status 
-     * 4 : when the message type is action 
-     * -1: when the message type is invalid
+     * @brief get message type
+     *
+     * @param[in] payload data sent by the client
+     *
+     * @return
+     * -  0: when the message type is handshakey
+     * -  1: when the message type is message
+     * -  2: when the message type is status
+     * -  4: when the message type is action
+     * - -1: when the message type is invalid
      */
     int getTypeMessage(std::uint8_t* payload);
 
     /**
-     * @brief get the client data that comes from the websocket client 
+     * @brief get the client data that comes from the websocket client
      * @param  payload data sent by the client
      * @param  client  where all customer data will be saved
      * @return esp_err_t
@@ -129,7 +131,7 @@ private:
     esp_err_t getClientData(std::uint8_t* payload, client_data_t* client);
 
     /**
-     * @brief function for sending asynchronous messages 
+     * @brief function for sending asynchronous messages
      * @param  payload data sent by the client
      * @param  client  where all customer data will be saved
      * @return esp_err_t
@@ -146,7 +148,7 @@ private:
     esp_err_t messageRecipient(std::uint8_t* payload, uid_message_t* uid_receiving, const char* null_to_uid);
     /**
      * @brief Verify that the data received is correct and send the message
-     * @param  client_uid contains the uid to which the message will be sent 
+     * @param  client_uid contains the uid to which the message will be sent
      * @param  ws_pkt  message sent by the client and that will be returned by the socket
      * @param  null_to_uid variable assigned to the toUID if it reaches null
      * @return esp_err_t
@@ -155,7 +157,6 @@ private:
 
     /**
      * @brief send pong messages to verify connected clients
-     * 
      */
     void pong(httpd_req_t* req);
 
