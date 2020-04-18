9a32953b6143b3b3a01b005fb2dd2606a8b88cf0
xkevin190 <kevinvelasco193@gmail.com>
2020-04-18 10:53:37 -0400

    added test to HTTP component

--- components/HTTP/test/testHttpServer.cpp
+++ components/HTTP/test/testHttpServer.cpp
@@ -6,10 +6,34 @@
 #include <unity.h>
 
 
+esp_err_t echoTest(httpd_req_t* req)
+{
+    return ESP_OK;
+}
+
 TEST_CASE("start the server", "[https server]")
 {
     http::HttpServer server_instance = http::HttpServer();
 
-    esp_err_t ret server_instance.start();
-    if (err != ESP_OK) TEST_FAIL();
+    esp_err_t ret = server_instance.start();
+    if (ret != ESP_OK) TEST_FAIL();
+}
+
+
+TEST_CASE("start the server", "[https server]")
+{
+    http::HttpServer server_instance = http::HttpServer();
+
+    esp_err_t ret = server_instance.start();
+    if (ret != ESP_OK) TEST_FAIL();
+}
+
+
+TEST_CASE("registering uri", "[https server]")
+{
+    http::HttpServer server_instance = http::HttpServer();
+
+    esp_err_t ret = server_instance.registerUri("/test", HTTP_GET, echoTest, NULL, false);
+
+    if (ret != ESP_OK) TEST_FAIL();
 }
