19ead6fc99f4899f0192f8376625f5862c561a6d
xkevin190 <kevinvelasco193@gmail.com>
2020-05-01 20:14:41 -0400

    added comments in credentials.h

--- components/NVS/src/Credentials.h
+++ components/NVS/src/Credentials.h
@@ -14,24 +14,45 @@
 #define NVS_CREDENTIALS_H
 
 #include "Storage.h"
-#include <iostream>
 #include "defaults.h"
 #include <cstring>
+#include <iostream>
 
-
-struct store_credentials_t 
-{
-  char nvs_username[MAX_USER_NAME_LENGTH];
-  char nvs_password[MAX_USER_PASSWORD_LENGTH];
+struct store_credentials_t {
+    char nvs_username[MAX_USER_NAME_LENGTH];
+    char nvs_password[MAX_USER_PASSWORD_LENGTH];
 };
 
 namespace credentials {
- 
- esp_err_t setInitialCredentials();
- esp_err_t getCredentials(store_credentials_t* user_credentials);
- bool credentialCompare(const char* a, const char* b );
- esp_err_t saveNewCredentials(store_credentials_t credentials); 
- 
- }
+/**
+ * @brief this adds the initial credentials in memory 
+ * 
+ */
+esp_err_t setInitialCredentials();
+
+/**
+ * @brief get credentials stored in memory 
+ * @param[out] user_credentials credentials returned
+ * 
+ * @return esp_err_t
+ */
+esp_err_t getCredentials(store_credentials_t* user_credentials);
+/**
+  * @brief this function verify credentials sent by the client with the credentials stored in memory 
+  * @param[in] a parameter to compare
+  * @param[in] b parameter to compare
+  * 
+  * @return bool
+  */
+bool credentialCompare(const char* a, const char* b);
+/**
+  * @brief this function save the username and password sent by client 
+  * @param[in] credentials 
+  * 
+  * @return esp_err_t
+  */
+esp_err_t saveNewCredentials(store_credentials_t credentials);
+
+} // namespace credentials
 
 #endif //
