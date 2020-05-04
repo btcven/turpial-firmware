eba372df0b8b5276918f7675a7f3c67e5e3ac89a
xkevin190 <kevinvelasco193@gmail.com>
2020-05-04 10:01:41 -0400

    removed decode function sendUart function

--- components/HTTP/src/Websocket.cpp
+++ components/HTTP/src/Websocket.cpp
@@ -354,7 +354,6 @@ esp_err_t Websocket::sendUart(httpd_ws_frame_t ws_pkt)
     cbor_encoder_close_container(&encoder, &map_encoder);
     std::size_t length = cbor_encoder_get_buffer_size(&encoder, buffer);
 
-    // websocketRadioRx(buffer, length);
     int cnt = radio::write(buffer, length);
     if (cnt < 0) {
         return ESP_FAIL;
