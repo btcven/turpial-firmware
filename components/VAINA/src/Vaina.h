c6d87817dbf38e0d02cfbb204ba49640d312b82f
xkevin190 <kevinvelasco193@gmail.com>
2020-05-13 12:53:05 -0400

    added comments

--- components/VAINA/src/Vaina.h
+++ components/VAINA/src/Vaina.h
@@ -17,17 +17,20 @@
 #include <esp_err.h>
 #include <iostream>
 
-
+/**
+ * 
+ * @brief client structure
+ */
 struct sta_data_t {
-    esp_ip4_addr_t sta_ipv4;
-    char sta_mac[13];
-    esp_ip6_addr_t sta_ipv6;
+    esp_ip4_addr_t sta_ipv4; // ipv4 address
+    char sta_mac[13];        // mac address
+    esp_ip6_addr_t sta_ipv6; // ipv6 address
 };
+
 
 struct mac_length_t {
     char mac[13];
 };
-
 
 /**
                     
@@ -47,9 +50,9 @@ class Vaina
 private:
     Vaina();
 
-    std::vector<sta_data_t> m_sta_connected;
-    std::vector<esp_ip4_addr_t> m_ipv4_array;
-    std::vector<mac_length_t> m_mac_array;
+    std::vector<sta_data_t> m_sta_connected;  // all data client array
+    std::vector<esp_ip4_addr_t> m_ipv4_array; // ipv4 array
+    std::vector<mac_length_t> m_mac_array;    // mac array
     /* data */
 public:
     friend network::WiFi
@@ -70,13 +73,61 @@ public:
         static Vaina instance;
         return instance;
     }
-
+    /**
+     * 
+     * @brief function executed when connecting a new client sta
+     * 
+     * @param[in] info information of new sta connected 
+     * 
+     * @return esp_err_t
+     */
     esp_err_t staConnected(wifi_event_ap_staconnected_t* info);
+
+    /**
+     * 
+     * @brief function executed when disconnect a client sta
+     * @param[in] info information of sta disconnect
+     * 
+     * @return esp_err_t
+     */
     esp_err_t staDisconected(wifi_event_ap_stadisconnected_t* info);
+    /**
+     * 
+     * @brief function executed when is generated new ipv4 address
+     * @param[in] info information of sta disconnect
+     * 
+     * @return esp_err_t
+     */
     void setArrayIpv4(esp_ip4_addr_t ipv4);
+    /**
+     * 
+     * @brief convert address ipv4 to ipv6
+     * @param[in] ipv4 ipv4 address
+     * 
+     * @return esp_err_t
+     */
     esp_ip6_addr_t ipv4ToIpv6(uint32_t* ipv4);
+    /**
+     * 
+     * @brief funtion to convert of decimal to hex 
+     * @param[in] n decimal numbers
+     *
+     * @return char*
+     */
     char* decToHexa(int n);
+    /**
+     * @brief executed when a client connected 
+     * @param[in] client client data
+     * 
+     * @return esp_err_t
+     */
     esp_err_t sendUartConnected(sta_data_t client);
+    /**
+     * @brief executed when a client disconnect
+     * @param[in] client client data
+     * 
+     * @return esp_err_t
+     */
     esp_err_t sendUartdisconnected(sta_data_t client);
 };
 
