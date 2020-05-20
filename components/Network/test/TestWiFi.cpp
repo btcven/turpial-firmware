fb10b9539e6ee6dd1ce45c16572dc7bc6412df08
xkevin190 <kevinvelasco193@gmail.com>
2020-05-20 19:11:14 -0400

    added unit test in the network component

--- components/Network/test/TestWiFi.cpp
+++ components/Network/test/TestWiFi.cpp
@@ -1,66 +1,103 @@
+
 /**
- * @file test_wifi_mode.cpp
- * @author Locha Mesh Developers (contact@locha.io)
- * @brief
- * @version 0.1
- * @date 2019-09-17
+ * Copyright 2020 btcven and Locha Mesh developers
  *
- * @copyright Copyright (c) 2019 Locha Mesh project developers
- * @license Apache 2.0, see LICENSE file for details
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include "Storage.h"
-#include "WiFi.h"
-#include "defaults.h"
+/**
+ * @ingroup     network
+ * 
+ * @file        test_wifi_mode.cpp
+ * @author      Locha Mesh Developers (contact@locha.io)
+ * @brief       Wi-Fi network interface
+ * 
+ */
 
+#include "Network/WiFi.h"
+#include "Storage.h"
+#include "defaults.h"
+#include <Network/Network.h>
+#include <cstring>
+#include <iostream>
 #include <unity.h>
 
-TEST_CASE("Wi-Fi initialize", "[network]")
+TEST_CASE("Network initialize", "[network]")
 {
-    const TickType_t DELAY = 10000 / portTICK_PERIOD_MS;
-
-    esp_err_t err;
-
-    esp_netif_init();
-
-    err = network::g_wifi_netif.init();
+    esp_err_t err = network::init();
     if (err != ESP_OK) TEST_FAIL();
-
-    wifi.setMode(WIFI_MODE);
-
-    network::APConfig ap_config = {
-        .ssid = WAP_SSID,
-        .password = WAP_PASS,
-        .authmode = WAP_AUTHMODE,
-        .max_conn = WAP_MAXCONN,
-        .channel = WAP_CHANNEL,
-    };
-
-    err = network::g_wifi_netif.setApConfig(ap_config);
-    if (err != ESP_OK) TEST_FAIL();
-
-    err = network::g_wifi_netif.start();
-    if (err != ESP_OK) TEST_FAIL();
-
-    vTaskDelay(DELAY);
 }
 
-TEST_CASE("Sanitize SSID", "[network]")
+
+TEST_CASE("Set mode WiFi", "[network]")
 {
-    std::uint8_t expected_buf[32] = {0};
-    expected_buf[0] = 'a';
-    expected_buf[1] = 'b';
-    expected_buf[2] = '?';
-    expected_buf[3] = 'c';
-
-    std::uint8_t buf[32] = {0};
-    buf[0] = 'a';
-    buf[1] = 'b';
-    buf[2] = 0x0E;
-    buf[3] = 'c';
-
-    esp_err_t err = network::sanitizeSsid(buf, 4);
+    esp_err_t err = network::netif_wifi.setMode(WIFI_MODE);
     if (err != ESP_OK) TEST_FAIL();
+}
 
-    TEST_ASSERT_EQUAL_MEMORY(expected_buf, buf, 32);
+
+TEST_CASE("Set ap config", "[network]")
+{
+    wifi_config_t ap_config;
+    ap_config.ap.ssid_len = 0;
+    memcpy(ap_config.ap.ssid, WAP_SSID, sizeof(WAP_SSID));
+    memcpy(ap_config.ap.password, WAP_PASS, sizeof(WAP_PASS));
+    ap_config.ap.authmode = WAP_AUTHMODE;
+    ap_config.ap.max_connection = WAP_MAXCONN;
+    ap_config.ap.channel = WAP_CHANNEL;
+    esp_err_t err = network::netif_wifi.setApConfig(ap_config);
+    if (err != ESP_OK) TEST_FAIL();
+}
+
+
+TEST_CASE("Set ap config", "[network]")
+{
+    wifi_config_t ap_config;
+    ap_config.ap.ssid_len = 0;
+    memcpy(ap_config.ap.ssid, WAP_SSID, sizeof(WAP_SSID));
+    memcpy(ap_config.ap.password, WAP_PASS, sizeof(WAP_PASS));
+    ap_config.ap.authmode = WAP_AUTHMODE;
+    ap_config.ap.max_connection = WAP_MAXCONN;
+    ap_config.ap.channel = WAP_CHANNEL;
+    esp_err_t err = network::netif_wifi.setApConfig(ap_config);
+    if (err != ESP_OK) TEST_FAIL();
+}
+
+
+TEST_CASE("set STA config", "[network]")
+{
+    wifi_config_t sta_config;
+    std::memcpy(sta_config.sta.ssid, WST_SSID, sizeof(WST_SSID));
+    std::memcpy(sta_config.sta.password, WST_PASS, sizeof(WST_PASS));
+    esp_err_t err = network::netif_wifi.setStaConfig(sta_config);
+    if (err != ESP_OK) TEST_FAIL();
+}
+
+TEST_CASE("checking if it is ap mode", "[network]")
+{
+    bool result = network::netif_wifi.isAp();
+    TEST_ASSERT_TRUE(result);
+}
+
+TEST_CASE("checking if it is STA mode", "[network]")
+{
+    bool result = network::netif_wifi.isSta();
+    TEST_ASSERT_TRUE(result);
+}
+
+
+TEST_CASE("stop WiFi", "[network]")
+{
+    esp_err_t err = = network::netif_wifi.stop();
+    if (err != ESP_OK) TEST_FAIL();
 }
