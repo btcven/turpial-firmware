16a73dd0a3e96074c1d52b00b3387db0588b6994
luisan00 <luisan00@hotmail.com>
2021-07-19 11:53:41 +0200

    Update aodvv2_reader.c

--- mesh/sys/net/aodvv2/aodvv2_reader.c
+++ mesh/sys/net/aodvv2/aodvv2_reader.c
@@ -16,7 +16,7 @@
  * @brief       RFC5444 RREP
  *
  * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
- * @author      Locha Mesh devs <contact@locha.io>
+ * @author      Locha Mesh developers <contact@locha.io>
  * @}
  */
 
