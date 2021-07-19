f93d1cfa9ab035b02aebdb6eab9d77b0d4b28685
luisan00 <luisan00@hotmail.com>
2021-07-19 11:53:18 +0200

    Update aodvv2_rcs.c

--- mesh/sys/net/aodvv2/aodvv2_rcs.c
+++ mesh/sys/net/aodvv2/aodvv2_rcs.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 Freie Universität Berlin
  * Copyright (C) 2014 Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
- * Copyright (C) 2021 btcven Locha Mesh developers
+ * Copyright (C) 2021 btcven and Locha Mesh developers
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -16,7 +16,7 @@
  * @brief       AODVv2 Router Client Set
  *
  * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
- * @author      Locha Mesh devs <contact@locha.io>
+ * @author      Locha Mesh developers <contact@locha.io>
  * @}
  */
 
